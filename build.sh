31281b20669cc541fb0e147d8a80d04492db7123
allarounddev <allarounddeveloper@outlook.com>
2023-07-23 17:16:50 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ set -o errexit
 python -m pip install --upgrade pip
 
 pip install -r "requirements.txt"
-pip install -U polygon-api-client
 
 python manage.py collectstatic --no-input
 python manage.py migrate
